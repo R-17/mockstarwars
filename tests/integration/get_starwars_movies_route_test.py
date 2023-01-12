8aae3bffa4910c0d55aae652b64ce08c3ff59ad3
R-17 <raphaelpavard17@.com>
2023-01-12 18:17:20 +0100

    update integratino tests

---
+++ tests/integration/get_starwars_movies_route_test.py
@@ -0,0 +1,10 @@
+import json
+from app import app
+
+
+def test_say_hello_route_return_say_property():
+    response = app.test_client().get('/character/1/movies')
+    data = json.loads(response.data.decode('utf-8'))
+
+    assert data['movies'] == [{"title": "A New Hope"}, {"title": "The Empire Strikes Back"}, {"title": "Return of the Jedi"}, {"title": "Revenge of the Sith"}, {"title": "The Force Awakens"} ]
+    assert response.status_code == 200
